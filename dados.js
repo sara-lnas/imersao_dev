21f07a7a11de42a97844d20bb3b94734bd4d610b
sara-nsantos <sara.santos@sptech.school>
2024-09-07 17:43:48 -0300

    adicionando personagens

--- dados.js
+++ dados.js
@@ -36,5 +36,70 @@ let dadosPersonagens = [
         descricao : "É o pai dos dois adolescentes, um homem inseguro e desmotivado, encontrava pouco alívio em seu trabalho monótono.",
         link: "https://rickandmorty.fandom.com/wiki/Jerry_Smith",
         tags : "pai inseguro humilhado bobo vulnerável imaturo negligente medroso genro burro depressivo"
+    },
+
+    {
+        foto : "img/persoangens/Poopybutthole.svg",
+        personagem : "Mr. Poopybutthole",
+        descricao : "O vizinho excêntrico da família Smith, que tem um passado misterioso e uma personalidade peculiar.",
+        link: "https://rickandmorty.fandom.com/wiki/Mr._Poopybutthole",
+        tags : "irritante vizinho"
+    },
+    
+    {
+        foto: "img/persoangens/squanchy.svg",
+        personagem : "Squanchy",
+        descricao : "O alienígena felino, que é um dos melhores amigos de Rick. Ele é conhecido por sua personalidade explosiva e seu amor por bebidas alcoólicas.",
+        link: "https://rickandmorty.fandom.com/wiki/Squanchy",
+        tags : "alienigena felino amigo traido gato bebado"
+    },
+
+    {
+        foto: "img/persoangens/birdperson.svg",
+        personagem: "Birdperson",
+        descricao: "O melhor amigo de cientista louco, que se transforma em um vilão após ser manipulado por Tammy.",
+        link: "https://rickandmorty.fandom.com/wiki/Birdperson",
+        tags : "amigo passaro bird traido"
+    },
+
+    {
+        foto: "img/persoangens/unity.svg",
+        personagem: "Unity",
+        descricao: "Uma inteligência artificial que assume a forma de uma mulher e se torna o interesse amoroso de Rick em uma realidade alternativa.",
+        link: "https://rickandmorty.fandom.com/wiki/Unity",
+        tags: "ia artificial paixao namorada"
+    },
+
+    {
+        foto: "img/persoangens/meeseeks.svg",
+        personagem: "Mr. Meeseeks",
+        descricao: "É uma criatura dimensional criada para cumprir uma única tarefa. Ela se torna cada vez mais frustrada e desesperada com o passar do tempo, caso não consiga completar sua missão.",
+        link: "https://rickandmorty.fandom.com/wiki/Mr._Meeseeks",
+        tags: "azul ansioso triste"
+    },
+
+
+    {
+        foto: "img/persoangens/deus.svg",
+        personagem: "Mr. Nimbus",
+        descricao: "O rei dos oceanos, um alienígena egocêntrico e poderoso que se envolve em uma relação complicada com o cientista.",
+        link: "https://rickandmorty.fandom.com/wiki/Mr._Nimbus",
+        tags: "peixe mar agua deus oceano rei "
+    },
+
+    {
+        foto: "img/persoangens/abradolf.svg",
+        personagem: "Abradolf Lincler",
+        descricao: "Uma versão alternativa de Adolf Hitler que se tornou um comediante de stand-up",
+        link: "https://rickandmorty.fandom.com/wiki/Abrodolph_Lincoler",
+        tags: "stand-up debochado engracado satira  "
+    },
+
+     {
+        foto: "img/persoangens/krombopulos.svg",
+        personagem: "Krombopulos Michael",
+        descricao: "É um assassino intergaláctico que se torna um aliado improvável e amogo de velho de cabeloa azuis.",
+        link: "https://rickandmorty.fandom.com/wiki/Krombopulos_Michael",
+        tags: "assasino intergalatico alienigena inseto"
     }
 ];
