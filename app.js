f6a49413cdf4e5cfd2a14c28e40f68f1408ad165
sara-nsantos <sara.santos@sptech.school>
2024-09-07 16:02:10 -0300

    integracao ddo background

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
 function pesquisar() {
 
     let section = document.getElementById("resultados-pesquisa");
-    let campoPesquisa = document.getElementById("campo-pesquisa").value; 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
 
     if (!campoPesquisa) {
         section.innerHTML = "<p>Nenhum personagem encontrado. Digite um nome válido.</p>"
         return 
     }
 
-    campoPesquisa = campoPesquisa.toLowerCase();
+    campoPesquisa = campoPesquisa.toLowerCase()
 
-    // Inicializa uma string vazia para armazenar os resultados 
+    // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
-    let personagem = "";
+    let personagem = ""; 
     let descricao = "";
     let tags = "";
-
-
+    
     // para cada dado dentro da lista, um personagem será selecionado
     for (let dado of dadosPersonagens) {
         personagem = dado.personagem.toLowerCase()
         descricao = dado.descricao.toLowerCase()
-
+        tags = dado.tags.toLowerCase()
 
         if (personagem.includes(campoPesquisa) || descricao.includes(campoPesquisa) 
             || tags.includes(campoPesquisa)) {
-
-        resultados += `
+       
+            resultados += `
             <div class="item-resultado">
                 <img src="${dado.foto}">
                 <div class="item-infomacoes"
-                    <h2>${dado.personagem}</h2>
+                    <h2 id="name-personagem">${dado.personagem}</h2>
                     <p class="descricao-meta">${dado.descricao}</p>
-                    <a href="${dado.link}" target="_blank"><button>Ver mais</button></a>
+                    <a href="${dado.link}" target="_blank">Ver mais</a>
                 </div>
             </div>
-            `;    
+        `;
         }
-    }  
-
-    if (!resultados)
-        resultados = "<p>Nada foi encontrado.</p>"
-
-    section.innerHTML = resultados
-
     }
 
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    section.innerHTML = resultados;
+} 
---
+++ dados.js
@@ -0,0 +1,40 @@
+let dadosPersonagens = [
+    {
+        foto : "img/persoangens/rick.svg",
+        personagem: "Rick Sanchez",
+        descricao: "O cientista brilhante e niilista, Rick, e seu neto inseguro, embarcam em loucas aventuras por múltiplos universos.",
+        link: "https://rickandmorty.fandom.com/wiki/Rick_Sanchez",
+        tags: "cientifico louco genio alcolico nilista satiro egoista impulsivo inventor interdimensional manipulador experimentador wubba lubba dub dub pickle rick"
+    },
+
+    {
+        foto : "img/persoangens/morty.svg",
+        personagem: "Morty Smith",
+        descricao: "O inseguro adolescente é o contraponto perfeitom avô em suas perigosas missões.",
+        link: "https://rickandmorty.fandom.com/wiki/Morty_Smith",
+        tags: "adolescente SciFi medroso inseguro vitima neto heroi descobridor aventureiro corajoso descobridor parceiro confidente dupla dinamica filho irmao"
+    },
+
+    {
+        foto : "img/persoangens/summer.svg",
+        personagem: "Summer Smith",
+        descricao: "A popular jovem esconde um lado mais inteligente e justo, frequentemente arrastada para as aventuras interdimensionais.",
+        link: "https://rickandmorty.fandom.com/wiki/Summer_Smith",
+        tags: "adolescente insegura vitima neta descobridora aventureira corajosa parceira confidente irma popular inteligente irresponsável superficial filha rebelde"
+    },
+
+    {
+        foto : "img/persoangens/beth.svg",
+        personagem : "Beth Smith",
+        descricao : "A cirurgiã equina luta com questões de identidade e se ressente por ter abandonado seus sonhos",
+        tags : "filha mae Cirurgia equina insatisfeita mulher forte solidária protetora"
+    },
+
+    {
+        foto : "img/persoangens/jerry.svg",
+        personagem : "Jerry Smith",
+        descricao : "É o pai dos dois adolescentes, um homem inseguro e desmotivado, encontrava pouco alívio em seu trabalho monótono.",
+        link: "https://rickandmorty.fandom.com/wiki/Jerry_Smith",
+        tags : "pai inseguro humilhado bobo vulnerável imaturo negligente medroso genro burro depressivo"
+    }
+];
