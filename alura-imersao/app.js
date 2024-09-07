398967ca620eba43766350e28cda2dc8174964b7
sara-nsantos <sara.santos@sptech.school>
2024-09-06 22:41:49 -0300

    tudi ta errado

--- alura-imersao/app.js
+++ alura-imersao/app.js
@@ -3,23 +3,44 @@ function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
     let campoPesquisa = document.getElementById("campo-pesquisa").value; 
 
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nenhum personagem encontrado. Digite um nome válido.</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    // Inicializa uma string vazia para armazenar os resultados 
+    let resultados = "";
     let personagem = "";
     let descricao = "";
-    let tags =   m "";
+    let tags = "";
 
-
-    let resultados = "";
 
     // para cada dado dentro da lista, um personagem será selecionado
-    for (let dado of dados) {
+    for (let dado of dadosPersonagens) {
+        personagem = dado.personagem.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+
+
+        if (personagem.includes(campoPesquisa) || descricao.includes(campoPesquisa) 
+            || tags.includes(campoPesquisa)) {
+
         resultados += `
             <div class="item-resultado">
-                <h2>${dado.titulo}.</h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href="${dado.link}" target="_blank"><button>Ver mais</button></a>
+                <img src="${dado.foto}">
+                <div class="item-infomacoes"
+                    <h2>${dado.personagem}</h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank"><button>Ver mais</button></a>
+                </div>
             </div>
-            `    
-    }
+            `;    
+        }
+    }  
+
+    if (!resultados)
+        resultados = "<p>Nada foi encontrado.</p>"
 
     section.innerHTML = resultados
 
